2e2a08d3f07aaf502c8ba605d16cc2a2e4b362e7
NyagakaEnock <nyagenox@gmail.com>
2017-02-17 10:01:57 +0300

    First Commit

--- FootballCombinator.php
+++ FootballCombinator.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html>
+<title>Football Combinator</title>
+<meta name="robots" content="noindex, nofollow">
+<body>
+<form method="post" action="FootballCombinator.php" id="shortener">
+<label for="longurl">Enter the No of Games</label>
+ <input type="text" name="NumberOfGames" id="longurl">
+ <input type="submit" value="Get all Combinations" name="submit">
+</form>
+
+
+</body>
+</html>
 <?php
 class FootballCombinator
 {
@@ -21,34 +35,37 @@ class FootballCombinator
 	
 	
 	//*Specify the No of Games Played
+	
 	public $NumberOfGames = 2;
 
 	
 	public function __construct()
 	{
-		
+
 	}
 	
 
-	public function ListCombinations()
+	public function ListCombinations($NumberOfGames)
 	{
 
-	echo $this->toTable($this->getTableValues($this->NumberOfGames));
+	echo $this->toTable($this->getTableValues($NumberOfGames));
 
 	}
 	public function getTableValues($count) {
 		if (1 === $count) {
 			// Win(1) and Draw(0)or Away Win(1) for the first variable
-			return array(array('1'), array('0'), array('-1'));
+			return array(array('Win'), array('Draw'), array('Lose'));
 		}   
 		$wins = $aways = $draws = $this->getTableValues(--$count);
+		
 		for ($i = 0, $total = count($wins); $i < $total; $i++) {
 			// the Win copy gets a 1 added to each row
-			array_unshift($wins[$i], '1');
+			array_unshift($wins[$i], 'Win');
 			// and the away win copy gets a -1
-			array_unshift($aways[$i], '-1');
+			array_unshift($aways[$i], 'Draw');
 			// and the away win copy gets a 0
-			array_unshift($draws[$i], '0');
+			array_unshift($draws[$i], 'Lose');
+			
 		}   
 
 		// combine the 1 and 0 and -1 copies to give this variable's output
@@ -56,13 +73,15 @@ class FootballCombinator
 	}
 
 	public function toTable(array $rows) {
+		$counter = 0;
 		$return = "<table>\n";
 		$headers = range('A', chr(64 + count($rows[0])));
+		
 		$return .= '<tr><th> Game ' . implode('</th><th> Game ', $headers) . "</th></tr>\n";
 
 		foreach ($rows as $row) {
-			
-			$return .= '<tr><td>' . implode('</td><td>', $row) . "</td></tr>\n";
+			$counter = $counter +1;
+			$return .= '<tr><td>' .$counter." ". implode('</td><td>', $row) . "</td></tr>\n";
 			
 		}
 
@@ -70,7 +89,22 @@ class FootballCombinator
 		
 	}
 }
+	$FootballCombinator = new FootballCombinator();
+		if(isset($_POST['submit']))
+		{
+			
+			if($_POST['NumberOfGames']==null)
+			{
+				$FootballCombinator->ListCombinations($FootballCombinator->NumberOfGames);
+			}else if(!is_numeric($_POST['NumberOfGames']))
+			{
+				echo ("Enter a valid Number");	
+			}else
+			{				
+		$FootballCombinator->ListCombinations($_POST['NumberOfGames']);				
+			}
+		}else{
+		$FootballCombinator->ListCombinations($FootballCombinator->NumberOfGames);	
+		}
 
-$FootballCombinator = new FootballCombinator();
-$FootballCombinator->ListCombinations();
 ?>
